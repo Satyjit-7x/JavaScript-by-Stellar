2ff85436d268aedfbe3bb41b4a1735f3601bceae
Satyjit-7x <chauhansatyjeet0203@gmail.com>
2024-10-16 17:59:14 +0530

    first.js

--- first.js
+++ first.js
@@ -1,1 +1,2 @@
-console.log("first my") // no semicolon required 
+console.log("first my") // no semicolon but javascript will run this programme because jScript is devloped in such way 
+// that it auto correct some of programmers syntax mistakes 
